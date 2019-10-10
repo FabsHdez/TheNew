42bf5acfcac39016c122caea13572589aa330407
FabsHdez <carlosfab@outlook.es>
2019-10-10 10:55:33 -0700

    Hot hice tal cosa la practica

---
+++ ConsoleApplication1/ConsoleApplication1/herramientasIfs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class herramientasIfs
+    {
+        /// <summary>
+        /// esta clase son las practicas de los ifs
+        /// </summary>
+        /// <returns></returns>
+        public static int algo() {
+            int r=0;
+            return r;
+
+                }
+    }
+}
